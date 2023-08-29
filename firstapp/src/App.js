8b2964bbc5925de33f6a3f9828ec6a760f0ede9e
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-08-29 16:32:04 +0100

    #

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,15 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './Card';
 
 
 function App() {
-  let name = "Albert";
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
+        <h1>Task: Add three Card elements</h1>
+        <Card h2="First card's h2" h3="First card's h3" />
+        <Card h2="Second card's h2" h3="Second card's h3" />
+        <Card h2="Third card's h2" h3="Third card's h3" />
     </div>
-  );
+  )
 }
-
 export default App;
+
+
+
+
+
+// import Heading from "./Heading";
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <Heading firstName="Albert" />
+//             <Heading firstName="Any name other than Albert!" />
+            
+//         </div>
+//     );
+// };
+
+// export default App;
+
