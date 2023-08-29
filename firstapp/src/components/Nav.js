a77d250ec1ca866cc721fd90e262277ba6ab2e40
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-08-29 12:58:30 +0100

    first commit

---
+++ firstapp/src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <Nav className="main-nav"> 
+      <ul>
+        <li><a href="/Home">Home</a></li>
+        <li><a href="/about">About</a></li> 
+        <li><a href="/contact">Contact</a></li> 
+      </ul>
+    </Nav>
+  );
+};
+
+export default Nav;
