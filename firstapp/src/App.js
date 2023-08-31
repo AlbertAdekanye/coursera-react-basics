4c749a29bf482bc017234e80fba6af0ee21d4c6d
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-08-31 12:02:48 +0100

    props drilling

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -16,28 +16,70 @@ import ModeToggler from './ModeToggler';
 // export default App;
 
 
+function Main(props) { 
+  return <Header msg={props.msg} />; 
+};
 
+function Header(props) { 
+  return ( 
+    <div style={{ border: "10px solid whitesmoke" }}> 
+      <h1>Header here</h1> 
+      <Wrapper msg={props.msg} /> 
+    </div> 
+  ); 
+};
 
+function Wrapper(props) { 
+  return ( 
+    <div style={{ border: "10px solid lightgray" }}> 
+      <h2>Wrapper here</h2> 
+      <Button msg={props.msg} /> 
+    </div> 
+  ); 
+};
 
+function Button(props) { 
+  return ( 
+    <div style={{ border: "20px solid orange" }}> 
+      <h3>This is the Button component</h3> 
+      <button onClick={() => alert(props.msg)}>Click me!</button> 
+    </div> 
+  ); 
+};
 
-
-  function App() {
-
-  function handleClick() {
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt('type a number');
-    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-  }
-  return (
-    <div>
-      <h1>Task: Add a button and handle a click event</h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3</button>
-    </div>
-  );
-}
+function App() { 
+  return ( 
+    <Main  
+      msg="I passed through the Header and the Wrapper and I reached the Button component"  
+    /> 
+  ); 
+}; 
 
 export default App;
+
+
+
+
+
+
+
+//   function App() {
+
+//   function handleClick() {
+//     let randomNum = Math.floor(Math.random() * 3) + 1;
+//     console.log(randomNum);
+//     let userInput = prompt('type a number');
+//     alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+//   }
+//   return (
+//     <div>
+//       <h1>Task: Add a button and handle a click event</h1>
+//       <button onClick={handleClick}>Guess the number between 1 and 3</button>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
